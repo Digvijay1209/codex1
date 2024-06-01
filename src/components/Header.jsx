e777aa115c24fc0c7f1189a36992333cec760888
Digvijay1209 <97652050+Digvijay1209@users.noreply.github.com>
2024-06-01 12:58:18 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+
+import {AppBar,Toolbar ,styled } from '@mui/material';
+
+
+const Container = styled(AppBar)`
+    background: #060606;
+    position: static;
+    border-bottom: 1px solid #2f2f2f;
+    height: 9vh;
+`;
+
+const Header=()=>
+    {
+        return(
+            <Container position='static'> 
+                <Toolbar>
+                 Codex
+                    
+                </Toolbar>
+            </Container>
+        )
+    }
+
+    export default Header;
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Code from "./code";
+import React from "react";
+import Result from "./Result";
+const Home=()=>{
+    return(
+        <>
+            <Header/>
+            <Code/>
+            <Result/>
+        </>
+    )
+}
+
+export default Home;
